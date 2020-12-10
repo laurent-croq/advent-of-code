53a2c92f5ca80cb50a6f24f8464cd9b81086dc84
Laurent CROQ <laurent.croq@orange.com>
2020-12-10 23:28:50 +0100

    Minor update on day10/1

--- 2020/day10.py
+++ 2020/day10.py
@@ -3,15 +3,15 @@
 import aoc
 puzzle_adapters = [ int(line) for line in aoc.read_puzzle_input() ]
 
-def find_longest(adapts, jolts):
-    if len(adapts) == 0:
+def find_longest(adapts, jolts=0):
+    if jolts == max(adapts)+3:
         return([])
     diff = 1 if jolts+1 in adapts else 3
-    return([diff] + find_longest(adapts[:adapts.index(jolts+diff)] + adapts[adapts.index(jolts+diff)+1:], jolts+diff))
+    return([diff] + find_longest(adapts, jolts+diff))
 
-longest = find_longest(puzzle_adapters, 0)
+longest = find_longest(puzzle_adapters)
 
-print("answer1 = %d" % (longest.count(1) * (longest.count(3)+1)))
+print("answer1 = %d" % (longest.count(1) * longest.count(3)))
 
 from itertools import groupby
 

c290c473b4aa4ef61faa75fd70db9999d6627bd6
Laurent CROQ <laurent.croq@orange.fr>
2020-12-12 16:40:21 -0600

    Add day13

---
+++ 2020/day13.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import aoc
+puzzle_lines = aoc.load_puzzle_input()
+
+earliest = int(puzzle_lines[0])
+buses = []
+departures = {}
+for t,b in enumerate(puzzle_lines[1].split(",")):
+    if b != "x":
+        buses.append(int(b))
+        departures[int(b)] = t
+
+if earliest != 0:
+    min_delay = None
+    for b in buses:
+        delay = b - earliest % b
+        if min_delay is None or delay<min_delay:
+            min_delay = delay
+            best_bus = b
+
+    print("answer1 = %d" % (min_delay*best_bus))
+
+def find_next(ts, period, next_bus):
+    next_ts = ts
+    while True:
+        next_ts += period
+        for b in [ buses[i] for i in range(buses.index(next_bus)+1) ]:
+            if (next_ts+departures[b]) % b != 0:
+                break
+        else:
+            break
+    
+    return(next_ts if next_bus == buses[-1] else find_next(next_ts, period*next_bus, buses[buses.index(next_bus)+1]))
+
+print("answer2 = %d" % find_next(0, buses[0], buses[1])) 

b9e4e2fe79f84db9fbba87e02fc8f15ff593214b
Laurent CROQ <laurent.croq@orange.fr>
2020-12-12 07:10:29 -0600

    Restore day11 (2nd star was missing)

--- 2020/day11.py
+++ 2020/day11.py
@@ -13,6 +13,21 @@ def new_seat_state1(occupied, x, y):
     s = sum(occupied[x+h][y+v] for h in range(-1,2) for v in range(-1,2)) - occupied[x][y]
     return(occupied[x][y] and s<4 or not occupied[x][y] and s==0)
 
+def new_seat_state2(occupied, x, y):
+    seen = 0
+    for dx,dy in [ [dx,dy] for dx in range(-1,2) for dy in range(-1,2) if [dx,dy] != [0,0] ]:
+        check_x = x+dx
+        check_y = y+dy
+        while check_x in range(len(seats)) and check_y in range(len(seats[0])) and not seats[check_x][check_y]:
+            check_x += dx
+            check_y += dy
+
+        if check_x in range(len(seats)) and check_y in range(len(seats[0])) and occupied[check_x][check_y]:
+            seen += 1
+            if not occupied[x][y] or seen == 5:
+                return(False)
+    return(True)
+
 def apply_rule(occupied, new_seat_state):
     new_occupied = copy.deepcopy(occupied)
 
@@ -21,11 +36,20 @@ def apply_rule(occupied, new_seat_state):
 
     return(new_occupied)
 
-occupied = list( [False]*len(seats[0]) for i in range(len(seats)) )
+occupied = list( [False]*len(seats[0]) for _ in range(len(seats)) )
 while True:
     new_occupied = apply_rule(occupied, new_seat_state1)
     if new_occupied == occupied:
         break
     occupied = new_occupied
 
-print("answser1 = %d" % sum(occupied[i][j] for i in range(1, len(seats)) for j in range(1, len(seats[0]))))
+print("answser1 = %d" % sum(sum(occupied, [])))
+
+occupied = list( [False]*len(seats[0]) for i in range(len(seats)) )
+while True:
+    new_occupied = apply_rule(occupied, new_seat_state2)
+    if new_occupied == occupied:
+        break
+    occupied = new_occupied
+
+print("answser2 = %d" % sum(sum(occupied, [])))

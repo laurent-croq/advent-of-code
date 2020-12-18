b299b4a88345d980c05860f7726067f08c48b588
Laurent CROQ <laurent.croq@orange.com>
2020-12-18 19:58:23 +0100

    Renaming

--- 2020/day18.py
+++ 2020/day18.py
@@ -32,13 +32,13 @@ import math
 def compute_part2(formula):
     return(math.prod([ 1 ] + [ sum([ int(i) for i in additions.split("+") ]) for additions in formula.split("*") ]))
     
-def matching_formula(formula, compute_func):
+def solve_formula(formula, compute_func):
     try:
         idx_open = formula.index("(")
         idx_close = matching_parenthesis(formula, idx_open+1)
-        return( matching_formula(formula[:idx_open] + str(matching_formula(formula[idx_open+1:idx_close], compute_func)) + formula[idx_close+1:], compute_func) )
+        return( solve_formula(formula[:idx_open] + str(solve_formula(formula[idx_open+1:idx_close], compute_func)) + formula[idx_close+1:], compute_func) )
     except:
         return(compute_func(formula))
 
-print("answer1 = %d" % sum(matching_formula(f, compute_part1) for f in formulas))
-print("answer2 = %d" % sum(matching_formula(f, compute_part2) for f in formulas))
+print("answer1 = %d" % sum(solve_formula(f, compute_part1) for f in formulas))
+print("answer2 = %d" % sum(solve_formula(f, compute_part2) for f in formulas))

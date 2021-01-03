9a536924d4cc86f406d4145e3c35418282ba2fa6
Laurent CROQ <laurent.croq@orange.com>
2021-01-03 22:05:04 +0000

    Add common base

---
+++ base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import aoc
+
+def puzzles(input_lines, **extra_args):
+    #for n in [ int(n) for n in input_lines ]
+    #for line in input_lines:
+    
+    yield(None)
+    yield(None)
+
+aoc.run(puzzles)
